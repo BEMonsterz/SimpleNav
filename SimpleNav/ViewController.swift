0ce8dfa45561777e5e4f3333896da61bf321d712
Bryan <bryanayllonroa@gmail.com>
2018-07-25 14:45:26 -0500

    Changed a lot of things

--- SimpleNav/ViewController.swift
+++ SimpleNav/ViewController.swift
@@ -7,13 +7,132 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
+   
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    var route: MKRoute!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.locationManager = CLLocationManager()
+        self.locationManager.delegate = self
+        self.mapView.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.requestAlwaysAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.mapView.showsUserLocation = true
+        //          // add gesture recognizer
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.mapLongPress(_:))) // colon needs to pass through info
+        longPress.minimumPressDuration = 1.5 // in seconds
+        //add gesture recognition
+        mapView.addGestureRecognizer(longPress)
+        
+        
     }
+    
+    
+//    func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D) {
+//
+//        let sourcePlacemark = MKPlacemark(coordinate: pickupCoordinate, addressDictionary: nil)
+//        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate, addressDictionary: nil)
+//
+//        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+//        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+//
+//        let sourceAnnotation = MKPointAnnotation()
+//
+//        if let location = sourcePlacemark.location {
+//            sourceAnnotation.coordinate = location.coordinate
+//        }
+//
+//        let destinationAnnotation = MKPointAnnotation()
+//
+//        if let location = destinationPlacemark.location {
+//            destinationAnnotation.coordinate = location.coordinate
+//        }
+//
+//        self.mapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
+//
+//        let directionRequest = MKDirectionsRequest()
+//        directionRequest.source = sourceMapItem
+//        directionRequest.destination = destinationMapItem
+//        directionRequest.transportType = .automobile
+//
+//        let directions = MKDirections(request: directionRequest)
+//
+//        directions.calculate {
+//            (response, error) -> Void in
+//
+//            guard let response = response else {
+//                if let error = error {
+//                    print("Error: \(error)")
+//                }
+//
+//                return
+//            }
+//
+//            let route = response.routes[0]
+//
+//            self.mapView.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+//
+//            let rect = route.polyline.boundingMapRect
+//            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+//        }
+//    }
+//
+//    //    MARK: - MKMapViewDelegate
+//
+//    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+//
+//        let renderer = MKPolylineRenderer(overlay: overlay)
+//
+//        renderer.strokeColor = UIColor(red: 17.0/255.0, green: 147.0/255.0, blue: 255.0/255.0, alpha: 1)
+//
+//        renderer.lineWidth = 5.0
+//
+//        return renderer
+//
+//
+//
+//
+//    }
+    @objc func mapLongPress(_ recognizer: UIGestureRecognizer) {
+        
+        print("A long press has been detected.")
+        
+        let touchedAt = recognizer.location(in: self.mapView) // adds the location on the view it was pressed
+        let touchedAtCoordinate : CLLocationCoordinate2D = mapView.convert(touchedAt, toCoordinateFrom: self.mapView) // will get coordinates
+        
+        let newPin = MKPointAnnotation()
+        newPin.coordinate = touchedAtCoordinate
+        mapView.addAnnotation(newPin)
+        
+        
+        
+    }
+
+    var isInitiallyZoomedToUserLocation: Bool = false
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if !isInitiallyZoomedToUserLocation {
+            isInitiallyZoomedToUserLocation = true
+            mapView.showAnnotations([userLocation], animated: true)
+            
+        }
+        
+    }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
